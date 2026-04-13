85d2929c3a6d31ed2f92b5c3128f6051237d28b3
106949 <1069461929@qq.com>
2026-04-13 17:24:20 +0800

    fix: 从版本库中移除误提交的 client_electron/node_modules

---
+++ client_electron/vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import electron from 'vite-plugin-electron'
+import renderer from 'vite-plugin-electron-renderer'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src/renderer'),
+    },
+  },
+  plugins: [
+    react(),
+    electron([
+      {
+        entry: 'src/main/index.ts',
+        vite: {
+          build: {
+            outDir: 'dist/main',
+          },
+        },
+      },
+      {
+        entry: 'src/preload/index.ts',
+        onstart(options) {
+          options.reload()
+        },
+        vite: {
+          build: {
+            outDir: 'dist/preload',
+          },
+        },
+      },
+    ]),
+    renderer(),
+  ],
+})
